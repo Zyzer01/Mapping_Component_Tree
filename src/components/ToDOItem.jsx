0c615d644307700b5089dbe49fcf5e45f6863440
David Onifade <davidicfola@gmail.com>
2023-03-10 23:09:28 +0100

    Initial commit

---
+++ src/components/ToDOItem.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ToDoItem(props) {
+  return (
+    <li
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      {props.text}
+    </li>
+  );
+}
